0def5e2af53848bfc27197b7edc8c750945a2a7c
cagri <muysal.cagri@gmail.com>
2022-12-20 17:58:47 +0300

    nvim: forgot to add these

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,30 @@
+-- leader <Space>
+vim.g.mapleader = " "
+
+-- cursor block
+vim.opt.guicursor = ""
+
+-- relative numbers for faster j,k
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- indenting
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- color
+vim.opt.termguicolors = true
+vim.opt.pumblend = 10
+
+vim.opt.title = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
