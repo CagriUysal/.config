a579006129223c5923926ebd80d0043579c8d48f
cagri <muysal.cagri@gmail.com>
2023-05-26 12:02:35 +0300

    add some keymaps

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>gs", "<cmd>vertical Git<CR>", { desc = "[G]it [S]tatus" })
 vim.keymap.set("n", "<leader>gm", "<cmd>Gvdiffsplit master<CR>")
+vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<CR>", { desc = "[G]it [B]lame" })
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -13,6 +13,8 @@ vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" 
 vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>sc", builtin.resume, { desc = "[S]earch [C]ontinue" })
+vim.keymap.set("n", "<leader>sr", builtin.lsp_references, { desc = "[S]earch [R]eferences" })
 
 vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
 vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
